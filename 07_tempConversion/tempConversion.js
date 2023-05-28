7a9071285f23b75ef91dd010d3788c7e96072244
Austin Wolff <austin.e.wolff@gmail.com>
2023-05-28 10:22:15 -0500

    Add functions which calc temp unit conversions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = 0;
+  if(typeof f === 'number'){
+    c = Math.round((f -32)*(5/9) * 10) / 10;
+    return c;
+  } else{
+    return 'error'
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = 0;
+  if(typeof c === 'number'){
+    f = Math.round(((c*(9/5) + 32)) * 10) / 10;
+    return f;
+  } else{
+    return 'error'
+  }
 };
 
 // Do not edit below this line
