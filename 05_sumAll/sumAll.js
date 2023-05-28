f820879ec9c21a085a2586dff6193b9a91a55d52
Austin Wolff <austin.e.wolff@gmail.com>
2023-05-28 09:55:34 -0500

    Add function to sum all values between two #s

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(intOne, intTwo) {
+    let returnVal = 0;
+    let start = 0;
+    let end = 0;
 
+    if(typeof intOne === 'number' && typeof intTwo === 'number'  && intOne >=0 && intTwo >=0 ){
+        if( intOne < intTwo){
+            start = intOne
+            end = intTwo
+        } else if(intTwo < intOne){
+            start = intTwo
+            end = intOne
+        } else{
+            return 0;
+        }
+        for(let i = start; i<= end; i++){
+            returnVal += i;
+        }
+    } else{
+        return 'ERROR';
+    }
+
+    return returnVal;
 };
 
 // Do not edit below this line
