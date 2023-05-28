d740f026232282b6995abe94d51aa0f1e4a93be7
Austin Wolff <austin.e.wolff@gmail.com>
2023-05-28 08:44:31 -0500

    Add a function that concatenates a string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, run) {
+    let returnString = '';
+    if(run < 0){
+        return 'ERROR';
+    } else{
+        for(let i = 0 ; i< run; i++){
+            returnString += string;
+        }
+    }
+    return returnString;
 };
 
 // Do not edit below this line
